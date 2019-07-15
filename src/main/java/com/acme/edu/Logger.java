88e79c87b708f6f4dfd5204a2f16b441b731db8a
Olga D <eve.oab@gmail.com>
2019-07-15 14:19:57 +0300

    First iteration of Logger update

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,20 +1,33 @@
 package com.acme.edu;
 
 public class Logger {
+    private static void printConsole(String hint){
+        System.out.println(hint);
+    }
+
+    private static String primitive = "primitive: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        printConsole(primitive + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        printConsole(primitive + message);
     }
 
-    public static void log(char message) {System.out.println("char: " + message);}
+    public static void log(char message) {
+        printConsole("char: " + message);
+    }
 
-    public static void log(String message) {System.out.println("string: " + message);}
+    public static void log(String message) {
+        printConsole("string: " + message);
+    }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);}
+        printConsole(primitive + message);
+    }
 
-    public static void log(Object message) {System.out.println("reference: " + message);}
+    public static void log(Object message) {
+        printConsole("reference: " + message);
+    }
 }
